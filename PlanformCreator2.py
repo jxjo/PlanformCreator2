03d7f77db553e2f1960fa1332d57aa185f2338f1
jxjo <58261241+jxjo@users.noreply.github.com>
2024-12-22 11:00:50 +0100

    prepare beta 3

--- PlanformCreator2.py
+++ PlanformCreator2.py
@@ -46,7 +46,7 @@ logger.setLevel(logging.DEBUG)
 #------------------------------------------------
 
 APP_NAME     = "Planform Creator 2"
-APP_VERSION  = "2.0 beta 2"
+APP_VERSION  = "2.0 beta 3"
 
 TEMPLATE_DIR = "templates"
 
