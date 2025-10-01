4151de16c5002c3c12775fea0dd56da21a40164d
jxjo <58261241+jxjo@users.noreply.github.com>
2025-10-01 13:41:53 +0200

    start 4.0

--- dev/subtree_AirfoilEditor_init.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo.
-echo Init Subtree AirfoilEditor
-echo.
-pause
-cd ..
-git subtree add --prefix AirfoilEditor_subtree https://github.com/jxjo/AirfoilEditor main --squash
-cd dev
-pause
--- dev/subtree_AirfoilEditor_pull.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo.
-echo Pull Subtree AirfoilEditor
-echo.
-pause
-cd ..
-git subtree pull --prefix AirfoilEditor_subtree https://github.com/jxjo/AirfoilEditor main --squash
-cd dev
-pause
